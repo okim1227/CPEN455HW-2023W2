9661520ddc2aa6dd7b9e0a36ebc240a13008470b
Olivia Kim <oliviakim@MacBook-Air-3.local>
2024-04-20 19:57:23 -0700

    included labels to sample in  generation_eval

--- generation_evaluation.py
+++ generation_evaluation.py
@@ -20,7 +20,11 @@ def my_sample(model, gen_data_dir, sample_batch_size = 25, obs = (3,32,32), samp
     for label in my_bidict:
         print(f"Label: {label}")
         #generate images for each label, each label has 25 images
-        sample_t = sample(model, sample_batch_size, obs, sample_op)
+
+        # generate labels as some tensor of ones
+        labels = torch.ones(sample_batch_size, dtype=torch.int64, device=device)*id
+        # added labels as param to sample
+        sample_t = sample(model, sample_batch_size, obs, sample_op, labels)
         sample_t = rescaling_inv(sample_t)
         save_images(sample_t, os.path.join(gen_data_dir), label=label)
     pass

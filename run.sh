9e4e541983d15c6226a7b053299e699a7cdcc084
Olivia Kim <oliviakim@MacBook-Air-3.local>
2024-04-22 11:57:47 -0700

    80 epoch hyperparam

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@ python pcnn_train.py \
 --batch_size 16 \
 --sample_batch_size 192 \
 --sampling_interval 20 \
---save_interval 49 \
+--save_interval 79 \
 --dataset cpen455 \
 --nr_resnet 1 \
 --nr_filters 40 \
 --nr_logistic_mix 5 \
 --lr_decay 0.999995 \
---max_epochs 50 \
+--max_epochs 80 \
 --en_wandb True \

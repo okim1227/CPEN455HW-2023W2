92c8e6cfa1ee800c4fa5f6537882b8b81bb7633b
Olivia Kim <oliviakim@MacBook-Air-3.local>
2024-04-21 18:42:08 -0700

    missing import

--- pcnn_train.py
+++ pcnn_train.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 from pprint import pprint
 import argparse
 from pytorch_fid.fid_score import calculate_fid_given_paths
+from classification_evaluation import classify
 
 
 def train_or_test(model, data_loader, optimizer, loss_op, device, args, epoch, mode = 'training'):

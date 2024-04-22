4d99335fc4f4f472297dd24ee5c8d798ab365c36
Olivia Kim <oliviakim@MacBook-Air-3.local>
2024-04-21 22:32:48 -0700

    hyperparams

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ python pcnn_train.py \
 --batch_size 16 \
 --sample_batch_size 192 \
 --sampling_interval 20 \
---save_interval 24 \
+--save_interval 49 \
 --dataset cpen455 \
 --nr_resnet 1 \
 --nr_filters 40 \

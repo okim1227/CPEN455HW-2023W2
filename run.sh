2adf46b4b0322b8f03e6855fbb12aa81fed4641e
Olivia Kim <oliviakim@MacBook-Air-3.local>
2024-04-21 21:44:06 -0700

    changed hyperparams

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@ python pcnn_train.py \
 --batch_size 16 \
 --sample_batch_size 192 \
 --sampling_interval 20 \
---save_interval 20 \
+--save_interval 50 \
 --dataset cpen455 \
 --nr_resnet 1 \
 --nr_filters 40 \
 --nr_logistic_mix 5 \
 --lr_decay 0.999995 \
---max_epochs 30 \
+--max_epochs 101 \
 --en_wandb True \
